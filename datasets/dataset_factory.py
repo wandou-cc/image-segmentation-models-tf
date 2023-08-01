b7828315b3ef96b6ecccc067db8adaa3177b6da1
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-08-01 15:25:27 +0000

    fix a mistake

--- datasets/dataset_factory.py
+++ datasets/dataset_factory.py
@@ -1,3 +1,10 @@
 """A factory-pattern class which returns classification image/label pairs."""
 
-from __future__ import absolute_import, divisi
+from __future__ import absolute_import, division, print_function
+
+from datasets import pascal
+
+datasets_map = {'pascal': pascal}
+
+
+def get_dataset(name, split_name, dataset_dir, file_pattern=None, reader=None):
