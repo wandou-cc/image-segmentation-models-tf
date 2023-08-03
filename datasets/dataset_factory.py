c634b1f66af9b0c5e991c261441f094afb2a235e
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-08-03 19:10:42 +0000

    iwlwifi: unregister LEDs if mac80211 registration fails

--- datasets/dataset_factory.py
+++ datasets/dataset_factory.py
@@ -10,4 +10,6 @@ datasets_map = {'pascal': pascal}
 def get_dataset(name, split_name, dataset_dir, file_pattern=None, reader=None):
   """Given a dataset name and a split_name returns a Dataset. """
   if name not in datasets_map:
-    raise ValueError('Name of dataset unknown %s
+    raise ValueError('Name of dataset unknown %s' % name)
+  return datasets_map[name].get_split(split_name, dataset_dir, file_pattern,
+                                      reade
