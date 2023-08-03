cbc4bf57dabfb291b901bfc94a5ee69fc2098b4c
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-08-03 16:05:28 +0000

    Header updates

--- datasets/dataset_factory.py
+++ datasets/dataset_factory.py
@@ -8,3 +8,6 @@ datasets_map = {'pascal': pascal}
 
 
 def get_dataset(name, split_name, dataset_dir, file_pattern=None, reader=None):
+  """Given a dataset name and a split_name returns a Dataset. """
+  if name not in datasets_map:
+    raise ValueError('Name of dataset unknown %s
