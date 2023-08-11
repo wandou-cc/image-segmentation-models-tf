0d5783db77357914f4b49649649d5c8cbdd57874
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-08-11 16:51:18 +0000

    remove unused options

--- datasets/dataset_utils.py
+++ datasets/dataset_utils.py
@@ -1,4 +1,17 @@
 """Contains utilities for downloading and converting datasets."""
 from __future__ import absolute_import, division, print_function
 
-import has
+import hashlib
+import os
+import sys
+import tarfile
+
+import glob2
+import tensorflow as tf
+
+from six.moves import urllib
+
+_RANDOM_SEED = 0
+
+
+def parse_glob(
