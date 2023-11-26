8e17348c6ad4fd9e1a1b9314f6853889db7db943
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-11-26 09:49:49 +0000

    Fixed Link

--- datasets/dataset_utils.py
+++ datasets/dataset_utils.py
@@ -133,4 +133,6 @@ def download(dataset_url, dataset_hash, dataset_dir):
 
   if not file_matches(filepath, dataset_hash):
     print("%s not found, downloading it!" % filepath)
-    filepath, _ = urllib.request.urlre
+    filepath, _ = urllib.request.urlretrieve(dataset_url, filepath, _progress)
+    statinfo = os.stat(filepath)
+    print('Successfully downloaded', filename, statinfo.st_si
