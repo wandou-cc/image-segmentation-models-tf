1645a1350bfe5fab008b28f20deab0609433690b
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2023-12-11 19:21:06 +0000

    forgot one

--- datasets/pascal.py
+++ datasets/pascal.py
@@ -4,4 +4,10 @@ import os
 
 import tensorflow as tf
 
-from datasets import data
+from datasets import dataset_utils
+
+slim = tf.contrib.slim
+
+_FILE_PATTERN = '%s_*.tfrecord'
+_CLASS_NAMES = [
+    'background', 'aeroplane', 'bicycle', 'bir
