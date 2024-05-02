af35606067468d35329eaf6ecc7d587b67bec64f
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2024-05-02 16:51:52 +0000

    Updated Canadian English translation.

--- nets/fcn.py
+++ nets/fcn.py
@@ -2,4 +2,11 @@
     arXiv:1605.06211
     https://github.com/shelhamer/fcn.berkeleyvision.org
 """
-fro
+from __future__ import absolute_import, division, print_function
+
+import tensorflow as tf
+
+slim = tf.contrib.slim
+
+
+def fcn_arg_scope(weight_decay=0.0005
