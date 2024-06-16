0a051e72162323d863de8db1eabb4909f74c3b5f
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2024-06-16 11:32:25 +0000

    Update htaccess

--- nets/layers.py
+++ nets/layers.py
@@ -2,4 +2,9 @@ import numpy as np
 
 
 def get_kernel_size(factor):
-  """Find the kernel si
+  """Find the kernel size given the desired factor of upsampling."""
+  return 2 * factor - factor % 2
+
+
+def upsample_filt(size):
+  """Make a 2D bilinear kernel suitable for upsa
