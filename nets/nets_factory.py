1b58f9579746c5ec428844e0cb71576202581eb9
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2024-06-24 09:06:09 +0000

    Actualizar README

--- nets/nets_factory.py
+++ nets/nets_factory.py
@@ -5,4 +5,6 @@ from __future__ import absolute_import, division, print_function
 import functools
 
 import tensorflow as tf
-i
+import tensorflow.contrib.slim as slim
+# from tensorflow.contrib.slim.nets import (alexnet, inception, overfeat, resnet_v1, resnet_v2, vgg)
+from nets import f
