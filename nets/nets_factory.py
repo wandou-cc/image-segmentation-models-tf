ef2a84210aee4e6c3dffb393b43d4f7af9fe208b
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2024-06-24 10:20:27 +0000

    2.0.0-beta.1

--- nets/nets_factory.py
+++ nets/nets_factory.py
@@ -7,4 +7,12 @@ import functools
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 # from tensorflow.contrib.slim.nets import (alexnet, inception, overfeat, resnet_v1, resnet_v2, vgg)
-from nets import f
+from nets import fcn
+
+slim = tf.contrib.slim
+
+networks_map = {
+    'fcn_32':
+        fcn.fcn_32,
+    # included in slim contrib
+    # 'alexnet_v2': alexnet.ale
