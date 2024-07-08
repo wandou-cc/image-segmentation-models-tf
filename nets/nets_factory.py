04cb818b596334b0e0adcfdce08fdac1460e540c
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2024-07-08 21:47:58 +0000

    add tags

--- nets/nets_factory.py
+++ nets/nets_factory.py
@@ -74,4 +74,7 @@ def get_network_fn(name, num_classes, weight_decay=0.0, is_training=False):
     Raises:
       ValueError: If network `name` is not recognized.
     """
-  if name
+  if name not in networks_map:
+    raise ValueError('Name of network unknown %s' % name)
+  arg_scope = arg_scopes_map[name](weight_decay=weight_decay)
+  func = n
