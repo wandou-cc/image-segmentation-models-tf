c402ca6e780788530e488a0a3ebf18a30f7f6c50
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2024-07-12 20:05:05 +0000

    Add .swift-version file

--- preprocessing/fcn_preprocessing.py
+++ preprocessing/fcn_preprocessing.py
@@ -1,1 +1,7 @@
-from __future__ import absol
+from __future__ import absolute_import, division, print_function
+
+import tensorflow as tf
+from tensorflow.python.ops import control_flow_ops
+from PIL import Image
+
+slim = tf.contrib.
