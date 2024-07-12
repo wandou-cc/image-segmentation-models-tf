70756b60e5283cb1eee71b0947b9781850ae3ced
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2024-07-12 15:34:05 +0000

    Translated using Translate.Akeeba (Turkish)

--- prepare_data.py
+++ prepare_data.py
@@ -15,3 +15,15 @@ flags.DEFINE_string('dataset_archive', None, 'where to find archives')
 
 def main(_):
   if not FLAGS.dataset_name:
+    raise ValueError('Must set --dataset_name')
+  if not FLAGS.dataset_dir:
+    raise ValueError('Must set --dataset_dir')
+  elif FLAGS.dataset_name == 'pascal':
+    download_and_convert_pascal.run(FLAGS.dataset_dir)
+
+  else:
+    raise ValueError('dataset_name [%s] not recognized.' % FLAGS.dataset_dir)
+
+
+if __name__ == '__main__':
+  tf.app.run()
