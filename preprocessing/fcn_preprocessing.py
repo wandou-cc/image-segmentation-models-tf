ed699e4b06f594dbd9c464908cdc3bd787bfd54a
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2024-07-12 21:41:07 +0000

    Initial project import.

--- preprocessing/fcn_preprocessing.py
+++ preprocessing/fcn_preprocessing.py
@@ -4,4 +4,13 @@ import tensorflow as tf
 from tensorflow.python.ops import control_flow_ops
 from PIL import Image
 
-slim = tf.contrib.
+slim = tf.contrib.slim
+
+# vgg network
+_R_MEAN = 123.68
+_G_MEAN = 116.78
+_B_MEAN = 103.94
+
+
+def _mean_image_subtraction(image, means):
+  if image.get_shape().ndims
