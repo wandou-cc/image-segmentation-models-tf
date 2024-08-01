9a2e0e837c0ce6d4b83c96652ecef3718b448c73
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2024-08-01 15:48:23 +0000

    auto commit page

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -5,4 +5,7 @@ python eval.py \
   --checkpoint_path=${TRAIN_DIR} \
   --eval_dir=${TRAIN_DIR} \
   --dataset_name=${DATASET} \
-  --dataset_dir=${DA
+  --dataset_dir=${DATASET_DIR} \
+  --model_name=${MODEL} \
+  --preprocessing_name=lenet \
+  --dataset_split_name=validation
---
+++ scripts/train.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Run training.
+python tra
