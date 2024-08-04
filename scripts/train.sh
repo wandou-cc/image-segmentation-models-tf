99938ce005482144ba366194ebdcce0be5168995
wandou-cc <90685660+wandou-cc@users.noreply.github.com>
2024-08-04 14:53:18 +0000

    rt2x00usb: fix indexes ordering on RX queue kick

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,4 +5,9 @@ python train.py \
   --dataset_name=${DATASET} \
   --dataset_dir=${DATASET_DIR} \
   --model_name=${MODEL} \
-  --save_summaries_se
+  --save_summaries_secs=60 \
+  --save_interval_secs=60 \
+  --dataset_split_name=train \
+  --preprocessing_name=lenet \
+  --max_number_of_steps=10000 \
+  --batch_siz
